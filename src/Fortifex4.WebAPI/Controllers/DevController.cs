d13106bce0155a9cae8edcaf1946097efe887200
Fuady Rosma Hidayat <fuady@live.com>
2020-06-01 14:11:24 +0700

    Sudah bisa dideploy ke server.vioren.com

---
+++ src/Fortifex4.WebAPI/Controllers/DevController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Fortifex4.WebAPI.Controllers
+{
+    public class DevController : ApiController
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IConfiguration _configuration;
+
+        public DevController(IWebHostEnvironment webHostEnvironment, IConfiguration configuration)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _configuration = configuration;
+        }
+
+        public ActionResult Index()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"ASPNETCORE_ENVIRONMENT: {_webHostEnvironment.EnvironmentName}");
+            sb.AppendLine($"AppContext.BaseDirectory: {AppContext.BaseDirectory}");
+            sb.AppendLine($"ConnectionString FortifexDatabase: {_configuration.GetSection("ConnectionStrings")["FortifexDatabase"]}");
+
+            return Content(sb.ToString());
+        }
+    }
+}
--- src/Fortifex4.WebAPI/Startup.cs
+++ src/Fortifex4.WebAPI/Startup.cs
@@ -1,42 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Fortifex4.WebAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -45,6 +39,7 @@ namespace Fortifex4.WebAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
             });
         }
     }
