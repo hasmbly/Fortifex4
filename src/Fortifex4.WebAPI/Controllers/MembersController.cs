010b3cbaf380c5baaeb5442ea2f02405d6925109
Muhammad Hasbi Ash Shiddieq <hasbi@vioren.com>
2020-07-13 14:56:40 +0700

    return exception dengan ApiEnvelope

--- src/Fortifex4.WebAPI/Controllers/MembersController.cs
+++ src/Fortifex4.WebAPI/Controllers/MembersController.cs
@@ -137,7 +137,9 @@ namespace Fortifex4.WebAPI.Controllers
             }
             catch (Exception exception)
             {
-                return StatusCode((int)HttpStatusCode.InternalServerError, new InternalServerError(exception));
+                return Ok(new InternalServerError(exception));
+
+                //return StatusCode((int)HttpStatusCode.InternalServerError, new InternalServerError(exception));
             }
         }
     }
