98d15a07360317c66a09c4591bfa7d95c58fe927
Fuady Rosma Hidayat <fuady@live.com>
2020-08-13 16:39:25 +0700

    Disable Migration from Program.cs

--- src/Fortifex4.WebAPI/Program.cs
+++ src/Fortifex4.WebAPI/Program.cs
@@ -1,10 +1,5 @@
-using System;
-using Fortifex4.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Fortifex4.WebAPI
 {
@@ -13,23 +8,6 @@ namespace Fortifex4.WebAPI
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-
-                try
-                {
-                    var fortifexDBContext = services.GetRequiredService<Fortifex4DBContext>();
-                    fortifexDBContext.Database.Migrate();
-                    logger.LogInformation("Fortifex4DB database has been successfully migrated.");
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex, "An error occurred while migrating or initializing the database.");
-                }
-            }
 
             host.Run();
         }
